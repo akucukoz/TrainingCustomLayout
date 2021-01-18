a80df7c26746d4dd00c922e74a32c10e1ed1e4cc
NarcHoz95 <axk042@shsu.edu>
2021-01-17 22:28:22 -0500

    Multiple children can be displayed at the same time

--- app/src/main/java/com/aranteknoloji/views/CustomLayout.kt
+++ app/src/main/java/com/aranteknoloji/views/CustomLayout.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.marginBottom
 import com.aranteknoloji.trainingcustomlayout.R
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -13,34 +14,48 @@ class CustomLayout(
     attributeSet: AttributeSet
 ) : ViewGroup(context, attributeSet) {
 
-    private lateinit var myText: TextView
-
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        myText = findViewById(R.id.my_text)
-    }
+//    override fun onFinishInflate() {
+//        super.onFinishInflate()
+//        myText = findViewById(R.id.my_text)
+//    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         measureChildren(widthMeasureSpec, heightMeasureSpec)
+        var layoutHeight = 0
+        var layoutWidth = 0
 
-        val lp = myText.layoutParams as MarginLayoutParams
-        val viewWidth = myText.measuredWidth + lp.leftMargin + lp.rightMargin
-        val viewHeight = myText.measuredHeight + lp.topMargin + lp.bottomMargin
+        println("childCount = $childCount")
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            val lp = view.layoutParams as MarginLayoutParams
+            val viewWidth = view.measuredWidth + lp.leftMargin + lp.rightMargin
+            val viewHeight = view.measuredHeight + lp.topMargin + lp.bottomMargin
 
-        val width = viewWidth + paddingLeft + paddingRight
-        val height = viewHeight + paddingTop + paddingBottom
+            val width = viewWidth + paddingLeft + paddingRight
+            val height = viewHeight + paddingTop + paddingBottom
 
-        setMeasuredDimension(View.resolveSize(width, widthMeasureSpec), View.resolveSize(height, heightMeasureSpec))
+            layoutHeight += height
+            if (width > layoutWidth) layoutWidth = width
+        }
+
+        setMeasuredDimension(View.resolveSize(layoutWidth, widthMeasureSpec), View.resolveSize(layoutHeight, heightMeasureSpec))
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val lp = myText.layoutParams as MarginLayoutParams
-        val left = paddingLeft + lp.leftMargin
-        val top = paddingTop + lp.topMargin
-        val right = left + myText.measuredWidth
-        val bottom = top + myText.measuredHeight
+        var recentBottom = 0
 
-        myText.layout(left, top, right, bottom)
+        for (i in 0 until childCount) {
+            val view = getChildAt(i)
+            val lp = view.layoutParams as MarginLayoutParams
+            val left = paddingLeft + lp.leftMargin
+            val top = paddingTop + lp.topMargin + recentBottom
+            val right = left + view.measuredWidth
+            val bottom = top + view.measuredHeight
+
+            recentBottom = bottom + lp.bottomMargin
+
+            view.layout(left, top, right, bottom)
+        }
     }
 
     // in order to generate the Layout Params
